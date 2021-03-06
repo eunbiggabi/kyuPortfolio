c38e0024207165f27f5ed8b88e03de9a3db14f4b
eunbiggabi <eunbiggabi84@gmail.com>
2021-03-06 21:40:11 +1000

    readme file update

--- index.js
+++ index.js
@@ -5,7 +5,11 @@
 const toggleBtn = document.querySelector(".navbar__toggle-btn");
 const menu = document.querySelector(".navbar__menu");
 
-toggleBtn.addEventListener("click", () => {
+toggleBtn.addEventListener("click", function() {
   menu.classList.toggle("active");
 });
+
+// toggleBtn.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+// });
 
