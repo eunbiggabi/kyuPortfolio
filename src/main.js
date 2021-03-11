9750ada6ab591c51d2e10460a2ac649f491d54bb
eunbiggabi <eunbiggabi84@gmail.com>
2021-03-11 19:59:11 +1000

    hope last touch

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+// Hamburger menu 
+
+
 let hamburger = document.getElementById("hamburger-menu")
 
 hamburger.addEventListener("click", () => {
     let menu = document.getElementById("menu")
     menu.classList.toggle("hidden")
 })
+
+
